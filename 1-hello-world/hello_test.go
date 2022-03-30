91d5980508d641cf93fceb782a2fd4aa82dd4d62
Ricky Chon <rickychon99@gmail.com>
2022-03-30 16:28:54 -0400

    Add multiple language support

--- 1-hello-world/hello_test.go
+++ 1-hello-world/hello_test.go
@@ -11,14 +11,26 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Person")
+		got := Hello("Person", "")
 		want := "Hello, Person"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("some dude", spanish)
+		want := "Hola, some dude"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("some dude", french)
+		want := "Bonjour, some dude"
 		assertCorrectMessage(t, got, want)
 	})
 }
